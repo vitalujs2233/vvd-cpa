d26f21908cb325c75f8c5fa195666345d4f61c0f
vitalujs2233 <44118467+vitalujs2233@users.noreply.github.com>
2026-06-30 15:30:48 +0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,5 @@
+document.querySelectorAll('.bottom button').forEach(b=>b.onclick=()=>{
+document.querySelectorAll('.page').forEach(p=>p.classList.remove('active'));
+document.getElementById(b.dataset.page).classList.add('active');
+});
 
