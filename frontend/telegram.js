43b52853f638319f1075598fc412327d968ad896
vitalujs2233 <44118467+vitalujs2233@users.noreply.github.com>
2026-06-30 15:31:26 +0300

    Update telegram.js

--- frontend/telegram.js
+++ frontend/telegram.js
@@ -1,1 +1,10 @@
+if(window.Telegram&&Telegram.WebApp){
+Telegram.WebApp.ready();
+Telegram.WebApp.expand();
+const u=Telegram.WebApp.initDataUnsafe.user;
+if(u){
+document.getElementById('username').textContent=u.first_name;
+if(u.photo_url)document.getElementById('avatar').src=u.photo_url;
+}
+}
 
