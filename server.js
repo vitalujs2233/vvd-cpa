dddeaff0e1d9100abe74022bddc80a9f57c60f92
vitalujs2233 <44118467+vitalujs2233@users.noreply.github.com>
2026-07-02 13:09:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log("🚀 VVD CPA запущен");
+    console.log(`http://localhost:${PORT}`);
+});
