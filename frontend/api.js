d0161cc10e99ac22f1dc87ec2d23e3c477dffba2
vitalujs2233 <44118467+vitalujs2233@users.noreply.github.com>
2026-07-02 13:01:24 +0300

    Delete frontend directory

--- frontend/api.js
+++
@@ -1,2 +0,0 @@
-const API="http://localhost:8000";
-async function getProfile(){return fetch(API+"/profile").then(r=>r.json())}
--- frontend/app.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.bottom button').forEach(b=>b.onclick=()=>{
-document.querySelectorAll('.page').forEach(p=>p.classList.remove('active'));
-document.getElementById(b.dataset.page).classList.add('active');
-});
-
