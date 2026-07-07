0653cc8c7517a61c40cc2e5c1131fcfbd39b1a0f
vitalujs2233 <44118467+vitalujs2233@users.noreply.github.com>
2026-07-07 17:01:30 +0300

    Update database.py

--- backend_v2/database.py
+++ backend_v2/database.py
@@ -8,3 +8,54 @@ def check_database():
     with engine.connect() as conn:
         conn.execute(text("SELECT 1"))
         return True
+
+
+def save_user(
+    telegram_id,
+    first_name,
+    last_name,
+    username,
+    photo_url
+):
+    with engine.begin() as conn:
+        conn.execute(
+            text("""
+            INSERT INTO users (
+                telegram_id,
+                first_name,
+                last_name,
+                username,
+                photo_url,
+                balance,
+                hold,
+                withdrawn,
+                role,
+                status
+            )
+            VALUES (
+                :telegram_id,
+                :first_name,
+                :last_name,
+                :username,
+                :photo_url,
+                0,
+                0,
+                0,
+                'user',
+                'active'
+            )
+            ON CONFLICT (telegram_id)
+            DO UPDATE SET
+                first_name = EXCLUDED.first_name,
+                last_name = EXCLUDED.last_name,
+                username = EXCLUDED.username,
+                photo_url = EXCLUDED.photo_url
+            """),
+            {
+                "telegram_id": telegram_id,
+                "first_name": first_name,
+                "last_name": last_name,
+                "username": username,
+                "photo_url": photo_url,
+            }
+        )
