1bc8b7b47e8c1fd7515e86aee3b71de7f61818ce
vitalujs2233 <44118467+vitalujs2233@users.noreply.github.com>
2026-07-08 14:45:05 +0300

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -17,8 +17,7 @@ const [balance, setBalance] = useState({
 useEffect(() => {
   const loadBalance = async () => {
     try {
-      const res = await fetch(
-        `https://vvd-cpa-v2.onrender.com/balance/${user.telegram_id}`
+      const res = await fetch(`https://vvd-cpa-v2.onrender.com/balance/${user.id}`)
       );
 
       const data = await res.json();
