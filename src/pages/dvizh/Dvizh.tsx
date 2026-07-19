0da433ac7363e478725912829f49383417f44ae7
vitalujs2233 <44118467+vitalujs2233@users.noreply.github.com>
2026-07-20 01:05:27 +0300

    Update Dvizh.tsx

--- src/pages/dvizh/Dvizh.tsx
+++ src/pages/dvizh/Dvizh.tsx
@@ -233,13 +233,10 @@ useEffect(() => {
 
   window.visualViewport?.addEventListener("resize", updateViewport);
 
-
-  return () => {
+return () => {
     window.visualViewport?.removeEventListener("resize", updateViewport);
 
-    if (window.Telegram?.WebApp) {
-  };
-
+    
   const containsForbiddenLink = (text: string): boolean => {
     const linkRegex = /(https?:\/\/|t\.me|telegram\.me|bit\.ly|tinyurl|cutt\.ly|vk\.com|instagram|facebook|youtube|discord|[a-zA-Z0-9-]+\.[a-zA-Z]{2,})/gi;
     return linkRegex.test(text);
